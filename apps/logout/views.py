229b96cc39fe760470a2994d57019906c3702d2c
micha <micha@DESKTOP-ODT1CIS>
2018-10-23 19:37:18 -0700

    Parnter Logout reset Pos FIx

--- apps/logout/views.py
+++ apps/logout/views.py
@@ -6,11 +6,8 @@ def LogoutCustom(request):
     user = request.user
     print(user.isPart)
     print (user.isVet)
-    if user.isPart:
-       user.partner.partner_Lat = 0.0
-       user.partner.partner_Lon = 0.0
-       user.partner.save()
-    elif user.isVet:
+
+    if user.isVet:
         user.veteran.vet_Lat = 0.0
         user.veteran.vet_Lon = 0.0
         user.veteran.save()
